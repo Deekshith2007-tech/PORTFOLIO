c49f46f6fdef69e31a3d117a572b11a73a788698
Deekshith2007-tech <deekshitharumugam@gmail.com>
2025-07-31 21:06:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-let display=document.getElementById('display');
-function append(value){
-    display.value+=value;
-}
-function clearDisplay(){
-    display.value='';
-}
-function deletelast(){
-    display.value=display.value.slice(0,-1);
-}
-function calculate(){
-    try{
-        display.value=eval(display.value);
-    }catch{
-        display.value='Error'
-    }
-}
+
